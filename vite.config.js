6fa22eae8a5077970f803fce433dab1513336db3
Niraj Kumar Tiwari <nirajtiwari0020@gmail.com>
2025-12-01 22:49:44 +0530

    initla chages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react()],
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
   }
 })
